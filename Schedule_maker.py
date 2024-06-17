64e87d7c1626b7efeb96c9dfbcb94d59f7d86679
StouAi <129324332+StouAi@users.noreply.github.>
2024-06-17 19:47:52 +0300

    Add the team list

---
+++ Schedule_maker.py
@@ -0,0 +1,24 @@
+#teams that are in the Serie A league (using 2023-2024 teams)
+
+teams = [
+    "ATA",  # Atalanta .
+    "BOL",  # Bologna .
+    "CAG",  # Cagliari .
+    "EMP",  # Empoli .
+    "FIO",  # Fiorentina .
+    "VER",  # H. Verona .
+    "INT",  # Inter Milan .
+    "JUV",  # Juventus .
+    "LAZ",  # Lazio .
+    "LEC",  # Lecce .
+    "MIL",  # AC Milan .
+    "MON",  # Monza .
+    "NAP",  # Napoli  .
+    "ROM",  # Roma .
+    "SAL",  # Salernitana .
+    "GEN",  # Genoa .
+    "SAS",  # Sassuolo .
+    "FRO",  # Spezia -
+    "TOR",  # Torino .
+    "UDI"   # Udinese .
+]
